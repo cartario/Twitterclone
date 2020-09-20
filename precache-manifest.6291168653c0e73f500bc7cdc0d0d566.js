511876f5e560479584856fb87610e3948e976e8c
VasiliyMac <cartario@yandex.ru>
2020-09-20 08:23:12 +0300

    Updates

---
+++ precache-manifest.6291168653c0e73f500bc7cdc0d0d566.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a423edf0345d596cf718bd7caf04f1d6",
+    "url": "/twitterclone/index.html"
+  },
+  {
+    "revision": "bb756ccea68c2161d8b0",
+    "url": "/twitterclone/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "606a84b4d06c9088cfbd",
+    "url": "/twitterclone/static/js/2.accf6c61.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/twitterclone/static/js/2.accf6c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb756ccea68c2161d8b0",
+    "url": "/twitterclone/static/js/main.5b4ae92c.chunk.js"
+  },
+  {
+    "revision": "2912e387ab97d36017a0",
+    "url": "/twitterclone/static/js/runtime-main.fcf2b215.js"
+  }
+]);

c1bafb28dbbb2faeed427a06e0bc7583a7d70892
cartario <cartario@yandex.ru>
2020-09-22 13:31:30 +0300

    homepage

--- src/home.jsx
+++ src/home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import Tweet from './tweet';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import { Container, createStyles } from '@material-ui/core';
+import { Container} from '@material-ui/core';
 import { Typography } from '@material-ui/core';
 import { IconButton } from '@material-ui/core';
-import { TextField } from '@material-ui/core';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import InputBase from '@material-ui/core/InputBase';
 import NotificationsIcon from '@material-ui/icons/NotificationsNoneOutlined';
@@ -14,7 +14,7 @@ import BookmarkIcon from '@material-ui/icons/BookmarkBorderOutlined';
 import RepeatIcon from '@material-ui/icons/RepeatOutlined';
 import UserIcon from '@material-ui/icons/PersonOutlineOutlined';
 import SearchIcon from '@material-ui/icons/SearchOutlined';
-
+import grey from '@material-ui/core/colors/grey';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -28,6 +28,14 @@ const useStyles = makeStyles((theme) => ({
   sideMenuItem: {
     display: `flex`,
     alignItems: `center`,
+
+    '&:hover': {
+      color: theme.palette.primary.main,
+    },
+
+    '&:hover svg path': {
+      fill: theme.palette.primary.main,
+    }
   },
   sideMenuItemLabel: {
     fontSize: 16,
@@ -36,10 +44,84 @@ const useStyles = makeStyles((theme) => ({
   sideMenuIcon: {
     fontSize: 30,
   },
-  logo: {
+  logo: {    
     fontSize: 32,
   },
+  tweetsWrapper: {
+    height: `100%`,
+    textAlign: `left`,
+    borderTop: 0,
+    borderBottom: 0,
+  },
+  tweetsHeader: {
+    padding: `10px`,
+    borderRight: 0,
+    borderLeft: 0,
+    borderTop: 0,
+  },
+  tweetsList: {
+    margin: 0,
+    padding: 0,
+    listStyle: `none`,
+  },
+  tweetsItem: {
+    padding: 10,
+    '&:hover': {
+      backgroundColor: `rgb(248,250,200)`
+    },
+    cursor: `pointer`,
+  },
+  tweetsUser: {
+    fontSize: 14
+  },
+  tweetsUserName: {
+    color: grey[500],
+  },
+  tweetIconButton: {
+    '&:hover': {
+      color: theme.palette.primary.main,
+    },
+  },
+  tweetIcon: {
+    fontSize: 18,         
+  },
+  tweetControls: {
+    display: `flex`,
+    justifyContent: `space-between`,
+    width: `80%`,
+    marginLeft: -13,
+  },
+  tweetAvatar: {
+    width: 40,
+    height: 40,
+  }
 }));
+
+const text = `The following npm package, @material-ui/icons, includes the 1,100+ official Material icons converted to SvgIcon components`;
+
+const users = [
+  {
+    _id: 'ddsdsds',
+    name: 'Natali',
+    email: '@natali',
+    text: '',
+    avatarUrl: `https://images.unsplash.com/photo-1537815749002-de6a533c64db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=845&q=80`
+  },
+  {
+    _id: `jhjhjkhk`,
+    name: 'Natali',
+    email: '@natali',
+    text: '',
+    avatarUrl: `https://images.unsplash.com/photo-1558898479-33c0057a5d12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80`
+  },
+  {
+    _id: `kokoooo`,
+    name: 'Natali',
+    email: '@natali',
+    text: '',
+    avatarUrl: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'
+  },
+];
 
 const BootstrapInput = withStyles((theme) => ({
   
@@ -62,7 +144,7 @@ const Home = () => {
           <Paper style={{height: `100vh`}} className={classes.paper}>            
             <ul className={classes.sideMenuList}>
               <li className={classes.sideMenuItem}>
-                <IconButton>
+                <IconButton style={{paddingTop: 0}}>
                   <TwitterIcon className={classes.logo} color="primary"/>
                 </IconButton>                
               </li>
@@ -106,7 +188,23 @@ const Home = () => {
           </Paper>       
         </Grid>
         <Grid item xs={6}>
-          <Paper style={{height: `100%`}} className={classes.paper}>xs</Paper>
+          <Paper className={classes.tweetsWrapper} variant="outlined">
+            <Paper className={classes.tweetsHeader} variant="outlined">
+              <Typography variant="h6">
+                Главная                
+              </Typography>
+              <form style={{height: `100px`}}>
+              </form>
+            </Paper>            
+            <ul className={classes.tweetsList}>
+              {users.map((user)=>
+                <Tweet key={user._id} classes={classes} text={text} user={user}/>
+              )}
+              
+              
+                      
+            </ul>
+          </Paper>
         </Grid>
         <Grid item xs={3}>
           {/* <Paper className={classes.paper}> */}
---
+++ src/tweet.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { Typography } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
+import RepeatIcon from '@material-ui/icons/RepeatOutlined';
+import Avatar from '@material-ui/core/Avatar';
+import CommentIcon from '@material-ui/icons/ChatBubbleOutlineOutlined';
+import LikeIcon from '@material-ui/icons/FavoriteBorderOutlined';
+import SendIcon from '@material-ui/icons/SendOutlined';
+
+const Tweet = ({classes, text, user}) => {
+  return (
+    <li className={classes.tweetsItem}>
+      <Grid container spacing={1}>
+        <Grid item xs={1}>
+          <Avatar className={classes.tweetAvatar} alt={`Аватарка пользователя ${user.name}`} src={user.avatarUrl} />
+        </Grid>
+        <Grid item xs={11}>
+          <Typography className={classes.tweetsUser}>
+          <b>{user.name}</b> <span className={classes.tweetsUserName}>{user.email}</span>
+          </Typography>
+          <Typography variant="body1">
+            {text}
+          </Typography>
+          <div className={classes.tweetControls}>
+            <IconButton className={classes.tweetIconButton}>
+              <CommentIcon className={classes.tweetIcon}/>
+              <span style={{fontSize: 14}}>1</span>
+            </IconButton>
+            <IconButton className={classes.tweetIconButton}>
+              <RepeatIcon className={classes.tweetIcon}/>                       
+            </IconButton>
+            <IconButton className={classes.tweetIconButton}>
+              <LikeIcon className={classes.tweetIcon}/>                        
+            </IconButton >
+            <IconButton className={classes.tweetIconButton}>
+              <SendIcon className={classes.tweetIcon}/>                        
+            </IconButton>                      
+          </div>
+        </Grid>
+      </Grid>
+    </li>
+  );
+};
+
+export default Tweet;
